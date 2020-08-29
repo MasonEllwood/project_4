8ae8115fbfe3c4db3f86cb384ba42833ad1b93e1
Mason Ellwood <masonellwood@gmail.com>
2020-08-28 17:07:12 -0700

    tickerprovider added

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:project_4/src/widgets/cat.dart';
 
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animation!'),
+      ),
+      body: buildAnimation(),
     );
+  }
+
+  Widget buildAnimation() {
+    return Cat();
   }
 }
